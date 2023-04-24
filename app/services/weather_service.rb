96512c2aab45ab40ab2c443a67435d5578ccf76c
Sam Walker <sgwalker327@gmail.com>
2023-04-24 09:22:02 -0600

    Got weather service working'

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,14 +1,21 @@
 class WeatherService
-
+  def initialize(location)
+    @location = location
+  end
   
+  def self.get_weather(location)
+    get_url("/v1/forecast.json?query=#{location}")
+  end
+
   def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def conn
-    Faraday.new("http://api.weatherapi.com/v1") do |f|
+  def self.conn
+    Faraday.new(url: "https://api.weatherapi.com/") do |f|
       f.params['key'] = ENV['WEATHER_API_KEY']
+      # f.params['q'] = @location
     end
   end
 end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe WeatherService do
   describe 'class methods' do
     context '.get_weather' do
-      
+      it 'returns weather data for a location' do
+
+        weather = WeatherService.get_weather('Denver, CO')
+        require 'pry'; binding.pry
+        expect(weather).to be_a(Hash)
+      end
     end
   end
 end

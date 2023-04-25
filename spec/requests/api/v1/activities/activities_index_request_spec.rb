e4923dc4556db4df25efb6ae0e2edb7db0d76bf5
Sam Walker <sgwalker327@gmail.com>
2023-04-25 11:50:29 -0600

    Added sad path test for activities index request spec

--- spec/requests/api/v1/activities/activities_index_request_spec.rb
+++ spec/requests/api/v1/activities/activities_index_request_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe 'Activities API', type: :request do
 
     expect(data).to be_a(Hash)
   end
+
+  it 'returns an error when no location present' do
+    location = ''
+    get "/api/v1/activities?destination=#{location}"
+
+    expect(response.status).to eq(400)
+
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error.keys).to include(:error)
+    expect(error[:error]).to eq('Invalid location')
+  end
 end

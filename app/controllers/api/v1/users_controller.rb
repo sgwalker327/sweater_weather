d75c74ca29d174e06798597c578a28ec650b3dd7
Sam Walker <sgwalker327@gmail.com>
2023-04-24 21:09:23 -0600

    Initial user controller, model, test setup

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::v1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    user.api_key = SecureRandom.hex(16)
+      if user.save
+        render json: UserSerializer.new(user), status: 201
+      else 
+        render json: { error: user.errors.full_messages }, status: 400
+      end
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates_confirmation_of :password
+
 end

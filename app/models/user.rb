0ff844892cc4200c7844fad4d6133cc886679b84
Sam Walker <sgwalker327@gmail.com>
2023-04-24 21:19:38 -0600

    Setup user db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
   validates_confirmation_of :password

3cf7822ae8e818ac9f764446fad701c9e15a9ae7
Sam Walker <sgwalker327@gmail.com>
2023-04-25 09:40:44 -0600

    Setup test for searching by activity type, test passing

--- spec/services/activities_service_spec.rb
+++ spec/services/activities_service_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe ActivitiesService do
   describe 'class methods' do
     it '.get_activities' do
-    activities = ActivitiesService.get_activities
-    require 'pry'; binding.pry
+    activities = ActivitiesService.get_activities('relaxation')
+    
+    expect(activities).to be_a(Hash)
+    expect(activities.keys).to eq([:activity, :type, :participants, :price, :link, :key, :accessibility])
     end
   end
 end

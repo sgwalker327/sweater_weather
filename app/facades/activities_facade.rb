803041460e0ab2904eccb6557ddf9cd60c907cf5
Sam Walker <sgwalker327@gmail.com>
2023-04-25 09:41:31 -0600

    Tweaked facade

--- app/facades/activities_facade.rb
+++ app/facades/activities_facade.rb
@@ -2,10 +2,13 @@ class ActivitiesFacade
   def activities(location)
     coords = LocationService.get_coordinates(location)
     weather = WeatherService.weather(coords[:lat], coords[:long])
-    activities = ActivitiesService.get_activities
-
+    
+    destination = "#{weather[:location][:name]}, #{weather[:location][:region]}"
     forecast = {
-      
+      summary: weather[:current][:condition][:text],
+      temperature: weather[:current][:temp_f]
     }
+    require 'pry'; binding.pry
+    acttivity1 = ActivitiesService.get_activities
   end
 end

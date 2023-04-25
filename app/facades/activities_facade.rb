bc33e8b9ed46f4e74abe251ee1be2518e6f90994
Sam Walker <sgwalker327@gmail.com>
2023-04-25 10:15:39 -0600

    Sorting out how to make this look right

--- app/facades/activities_facade.rb
+++ app/facades/activities_facade.rb
@@ -8,7 +8,24 @@ class ActivitiesFacade
       summary: weather[:current][:condition][:text],
       temperature: weather[:current][:temp_f]
     }
+    activity_1 = ActivitiesService.get_activities('relaxation')
+    activity_2 = (
+      if weather[:current][:temp_f] < 60 && weather[:current][:temp_f] >= 50
+        ActivitiesService.get_activities('busywork')
+      elsif weather[:current][:temp_f] >= 60
+        ActivitiesService.get_activities('relaxation')
+      else
+        ActivitiesService.get_activities('cooking')
+      end
+    )
+    activities = [activity_1, activity_2]
+    hashed = activities.map do |activity|
+                [:activity] {
+                  type: activity[:type],
+                  participants: activity[:participants],
+                  price: activity[:price]
+                }
+              end
     require 'pry'; binding.pry
-    acttivity1 = ActivitiesService.get_activities
   end
 end

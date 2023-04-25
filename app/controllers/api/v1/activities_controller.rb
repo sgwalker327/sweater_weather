3089cc1d0a38c146e329b2903be592006504fd89
Sam Walker <sgwalker327@gmail.com>
2023-04-25 10:28:30 -0600

    Slight edit to controller

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ActivitiesController < ApplicationController
   def index
-    x = ActivitiesFacade.activities(params[:destination])
+    x = ActivitiesFacade.new.activities(params[:destination])
     require 'pry'; binding.pry
   end
 end
